fca145ca41bbb764fd975fee4486b5cf56634c80
Paulo Lima <eng.paulocosta@icloud.com>
2021-05-06 19:34:21 -0300

    Calculando o IMC e manipulando o HTML

--- js/principal.js
+++ js/principal.js
@@ -2,11 +2,20 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelector(".paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 
 var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
+var tdImc = paciente.querySelector(".info-imc");
 
-var imc = peso/(altura*altura);
+var imc = peso/(Math.pow(altura,2));
+
+tdImc.textContent = imc;
+
+
+
+console.log(imc);
+
+
