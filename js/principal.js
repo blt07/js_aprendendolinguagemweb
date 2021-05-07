4647c4734def1a8fc6bf6080a841a342a504273d
Paulo Lima <eng.paulocosta@icloud.com>
2021-05-07 20:52:59 -0300

    Atribuindo estilos utilizando a funcao classlist no JS

--- js/principal.js
+++ js/principal.js
@@ -19,12 +19,14 @@ for(var i = 0; i < pacientes.length; i++){
     if(peso <= 0 || peso >= 1000){
         pesoEhValido = false;
         tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
     
     }
 
     if(altura <= 0 || altura >= 3){
         alturaEhValido = false
         tdImc.textContent = "Altura Inválida!";
+        paciente.classList.add("paciente-invalido");
     }
 
     if(alturaEhValido != false && pesoEhValido!=false){
